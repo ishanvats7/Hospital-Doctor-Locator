d1736526bd369f1088345509407cfa2eaac8a33b
ishanvats7 <ishan.vats18@gmail.com>
2025-10-24 20:16:02 +0530

    wrapped login and register page inside authprovider

--- Hospital-Locator/src/App.js
+++ Hospital-Locator/src/App.js
@@ -4,16 +4,19 @@ import SearchPage from "./pages/SearchPage";
 import HospitalPage from "./pages/HospitalPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./pages/context/AuthContext";
 
 function App() {
   return (
     <Router>
+      <AuthProvider>
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route path="/hospital/:id" element={<HospitalPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
+      </AuthProvider>
     </Router>
   );
 }
